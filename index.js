855c61686d03bf3293a7927530ee9acd38fe911f
kamalov-eldar <kamalov-eldar@mail.ru>
2023-09-07 12:07:54 +0300

    fix style button

--- index.js
+++ index.js
@@ -1,18 +1,7 @@
-let allBtn = document.querySelectorAll(".btn");
-allBtn[0].setAttribute("style", "position: absolute; top:66%; left: 23%;"); // Top714px Left:446px
-allBtn[1].setAttribute("style", "position: absolute; top:30%; left: 30%;");
-allBtn[2].setAttribute("style", "position: absolute; top:50%; left: 50%;");
-allBtn.forEach(
-    (btn) =>
-        (btn.onclick = function () {
-            this.classList.toggle("show-label");
-            btn.style.top = "";
-        }),
-);
-
-function inputPeriod() {
-    let period = document.querySelector(".slider-period__input");
-
-    /*  period.style.background =
-    "-webkit-linear-gradient(left, #68b738 0%, #68b738 " + period.value + "%, #cacaca " + period.value + "%, #cacaca 0%"; */
-}
+window.addEventListener("load", function () {
+    document.onclick = myClickHandler;
+    function myClickHandler(e) {
+        const parent = e.target.closest(".btn").classList.toggle("active");
+        console.log("e.target: ", e.target);
+    }
+});
