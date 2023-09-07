dc2b7605abef8fa47645bac750366ce6273ad947
kamalov-eldar <kamalov-eldar@mail.ru>
2023-09-07 17:16:23 +0300

    fix button click

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 window.addEventListener("load", function () {
     document.onclick = myClickHandler;
+    let prevButton = null;
+
     function myClickHandler(e) {
-        const parent = e.target.closest(".btn").classList.toggle("active");
-        console.log("e.target: ", e.target);
+        const button = e.target.closest(".btn");
+        if (button) {
+            if (button === prevButton) {
+                button.classList.toggle("active");
+            } else {
+                button.classList.add("active");
+                if (prevButton) prevButton.classList.remove("active");
+                prevButton = button;
+            }
+        } else {
+            if (prevButton) prevButton.classList.remove("active");
+        }
     }
 });
